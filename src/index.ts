f841de4cee39c16cc20ba95570e66d9d7270cd44
Léo Godard <leo@legodard.fr>
2022-04-23 03:30:00 +0200

    Add index.ts

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import * as Sonar from './sonar';
+import * as Github from './github';
+
+
+(async () => {
+    try {
+        if (!process.env.SONAR_ID || !process.env.SONAR_URL || !process.env.GITHUB_REF || !process.env.GITHUB_REPOSITORY || !process.env.APP_ID || !process.env.APP_KEY)
+            throw new Error('Environment variables are not set');
+        const measure = await Sonar.getMeasure(process.env.SONAR_ID)
+        Github.sendQualityReport(Sonar.fromMeasureGetQAReport(measure));
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    }
+})();
